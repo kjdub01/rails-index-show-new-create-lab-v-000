3755b8734488d847ee7716904128b1298d9a95f5
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-10 04:53:19 +0000

    Automatically backed up by Learn

--- db/migrate/20190210045210_create_coupons.rb
+++ db/migrate/20190210045210_create_coupons.rb
@@ -1,6 +1,10 @@
 class CreateCoupons < ActiveRecord::Migration
   def change
     create_table :coupons do |t|
+      t.string :coupon 
+      t.string :store
+      
+      times
     end
   end
 end
