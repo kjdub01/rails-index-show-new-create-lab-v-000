8761e82e84ec454ed4de94a0014c12515294c25d
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-10 04:55:34 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190210045210) do
-
-  create_table "coupons", force: :cascade do |t|
-    t.string   "coupon"
-    t.string   "store"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
