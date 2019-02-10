5f4fb9223b8694d16718c679f1d3f50a2aeb3c4b
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-10 05:05:31 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,14 @@
 class CouponsController < ApplicationController
   
+  def index 
+    @coupons = Coupon.all
+  end
+  
+  def show 
+    @coupon = Coupon.find_by(id: params[:id])
+  end
+  
+  
   def new
   end
   
